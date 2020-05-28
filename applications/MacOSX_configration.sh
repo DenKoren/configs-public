c8dc4883455cd0714f617af254783a7d1fb17f64
Korenevskiy Denis <denkoren@ozon.ru>
2020-05-28 20:48:57 +0300

    Make fish to be my default shell in newly installed systems

--- applications/MacOSX_configration.sh
+++ applications/MacOSX_configration.sh
@@ -1,2 +1,10 @@
 # Turn of mouse speed scaling (acceleration)
 defaults write GlobalPreferences com.apple.mouse.scaling -1
+
+# Allow fish to be default shell for Mac OS X users
+if ! grep -q /usr/local/bin/fish /etc/shells; then
+    sudo tee -a /etc/shells <<<"/usr/local/bin/fish"
+fi
+
+# Change default shell of current user to fish installed by brew
+chsh -s /usr/local/bin/fish
