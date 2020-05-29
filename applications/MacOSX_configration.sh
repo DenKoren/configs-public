2bafb94a379993ef8c6a5aade6d2adc727839487
Korenevskiy Denis <denkoren@ozon.ru>
2020-05-29 12:05:10 +0300

    Print tip about firefox tree style tab configuration after Mac OS X setup

--- applications/MacOSX_configration.sh
+++ applications/MacOSX_configration.sh
@@ -8,3 +8,25 @@ fi
 
 # Change default shell of current user to fish installed by brew
 chsh -s /usr/local/bin/fish
+
+cat <<EndOfTip
+To setup Mozilla Firefox for Tree Style Tab extension:
+
+* Open Firefox -> Help -> Troubleshooting information
+* Copy profile path
+* Create <firefox profile path>/chrome/userChrome.css file with contents:
+  ```
+  /* hides the native tabs */
+  #TabsToolbar {
+    visibility: collapse;
+  }
+
+  /* hides window title with close/expand buttons */
+  #titlebar {
+    visibility: collapse;
+  }
+  ```
+* Open about:config
+* Change toolkit.legacyUserProfileCustomizations.stylesheets to true
+* Restart Firefox
+EndOfTip
