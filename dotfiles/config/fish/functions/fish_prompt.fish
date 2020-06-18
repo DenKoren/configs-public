da36286266b30edb0c410cbdff5c18f219028eff
Korenevskiy Denis <denkoren@ozon.ru>
2020-06-18 19:46:14 +0300

    Actualize main packages and apps list, support arc util in fish_prompt script

--- dotfiles/config/fish/functions/fish_prompt.fish
+++ dotfiles/config/fish/functions/fish_prompt.fish
@@ -1,3 +1,5 @@
+# vim: set noexpandtab:
+
 function fish_prompt --description 'Write out the prompt'
 	set -l last_status $status
 
@@ -95,11 +97,19 @@ function fish_prompt --description 'Write out the prompt'
 		echo -n -s " "
 	end
 
+	function __fish_arc_prompt --description "Check if arc prompt generator is available and use it"
+		if type -q arc-prompt
+			echo -n -s " "
+			arc-prompt
+		end
+	end
+
 	echo -n -s \
 		"[$current_time]" ' '\
 		(set_color $color_cwd) (prompt_pwd) $normal \
 		(set_color white) "$current_py_venv" $normal \
-		(__fish_git_prompt) $normal \
+		(fish_git_prompt) $normal \
+		(__fish_arc_prompt) $normal \
 		\n \
 		(set_color $fish_color_user) "$USER" $normal \
 		@ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' '\
